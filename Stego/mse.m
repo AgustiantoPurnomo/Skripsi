22b8f08fa8594067eaebdecdfd1463bcb155915c
billy_fanino <billy.fanino24@gmail.com>
2018-05-13 19:29:03 +0700

    update bulk calculation

--- Stego/mse.m
+++ Stego/mse.m
@@ -3,13 +3,17 @@ selpath2 = uigetdir;
 for gbr=1:20
     path1 = strcat(selpath1,'\',int2str(gbr),'.bmp');
     image = imread(path1);
-    path2 = strcat(selpath2,'\',int2str(gbr),'.bmp');
+    path2 = strcat(selpath2,'\dwt-',int2str(gbr),'.bmp');
     image2 = imread(path2);
     n=size(image);
     M=n(1);
     N=n(2);
     MSE = sum(sum((image-image2).^2))/(M*N);
     PSNR = 10*log10(256*256/MSE);
-    resultmse(gbr) = a;
-    resultpsnr(gbr) = a;
+    %fprintf('\nMSE: %7.2f ', MSE);
+    %fprintf('\nPSNR: %9.7f dB', PSNR);
+    resultmse(gbr) = MSE(1); %dwt
+    resultpsnr(gbr) = PSNR(1); %dwt
+    %resultmse(gbr) = mean(MSE); %lsb
+    %resultpsnr(gbr) = mean(PSNR); %lsb
 end
